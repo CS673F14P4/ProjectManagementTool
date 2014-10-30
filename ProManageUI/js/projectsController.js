31063d524c137fe5751e65cefbe12e94ad605f66
marianavsarantes <marianavsarantes@gmail.com>
2014-10-30 11:28:33 -0400

    Small chenges in html just for appearence and js file to get list of projects from server.

---
+++ ProManageUI/js/projectsController.js
@@ -0,0 +1,19 @@
+/**
+ * author: Mariana Arantes 
+ */
+
+/* Get list of projects */
+/* 
+function projectsController($scope,$http) {
+  $http.get("http://localhost:8080/ProManageREST/jaxrs/project")
+  .success(function(response) {$scope.projects = response;});
+}*/
+
+/* just for test */
+function projectsController($scope) {
+
+  $scope.projects = [{"description": "Bla bla bla about projetc A", "name": "Project A"},
+                     {"description": "Bla bla bla about projetc B", "name": "Project B"},
+                     {"description": "Bla bla bla about projetc C", "name": "Project C"},
+                     {"description": "Bla bla bla about projetc D", "name": "Project D"}];
+}
