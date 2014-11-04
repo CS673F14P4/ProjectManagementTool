180c6e82b47f221b05aacde75515e253f59ee994
Nethra Balasundaram <nethra.pd41@gmail.com>
2014-11-04 18:44:04 -0500

    update

--- ProManageDAO/src/bu/met/cs/cs673/pm/dao/ProjectDAO.java
+++ ProManageDAO/src/bu/met/cs/cs673/pm/dao/ProjectDAO.java
@@ -1,7 +1,121 @@
 package bu.met.cs.cs673.pm.dao;
 
-public class ProjectDAO {
-	
-	//some change in the code
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import bu.met.cs.cs673.pm.dto.Project;
+
+/**
+ * 
+ * @author Luis Marion
+ */
+public class ProjectDAO 
+{
+
+	public int createProject(Project project)
+	{
+		int success = -1;
+		
+		if (project == null)
+		{
+			success = -1;
+		}
+		else
+		{
+			SqlSessionFactory factory = SessionFactorySingleton.getInstance().getSqlSessionFactory();
+			SqlSession session = factory.openSession();
+			
+			try 
+			{
+				success = session.insert("insertProject", project);
+				session.commit();
+			} 
+			finally 
+			{
+			  session.close();
+			}
+		}
+	
+	
+		if (success > 0)
+		{
+			success = project.getId();
+		}
+		
+		return success;
+	}
+	
+	public Project getProject(int projectId)
+	{
+		System.out.println("getProject");
+		Project project = new Project();
+		
+		SqlSessionFactory factory = SessionFactorySingleton.getInstance().getSqlSessionFactory();
+		SqlSession session = factory.openSession();
+		
+		try 
+		{
+			project = (Project) session.selectOne("getProject", projectId);
+		} 
+		finally 
+		{
+		  session.close();
+		}
+		
+		return project;
+	}
+	
+	public List<Project> getProjects()
+	{
+		List<Project> projects = null;
+		
+		SqlSessionFactory factory = SessionFactorySingleton.getInstance().getSqlSessionFactory();
+		SqlSession session = factory.openSession();
+		
+		try 
+		{
+			projects = session.selectList("getProjectsByUser");
+		} 
+		finally 
+		{
+		  session.close();
+		}
+		
+		return projects;
+	}
+	
+	public boolean updateProject()
+	{
+		return false;
+	}
+	
+	public boolean deleteProject(int projectId)
+	{
+		boolean success = false;
+		
+		int rows = -1;
+		
+		SqlSessionFactory factory = SessionFactorySingleton.getInstance().getSqlSessionFactory();
+		SqlSession session = factory.openSession();
+		
+		try 
+		{
+			rows = session.delete("deleteProject", projectId);
+			session.commit();
+		} 
+		finally 
+		{
+		  session.close();
+		}
+		
+		if (rows > 0)
+		{
+			success = true;
+		}
+		
+		return success;
+	}
+	
 }
--- ProManageDAO/src/bu/met/cs/cs673/pm/dto/Project.java
+++ ProManageDAO/src/bu/met/cs/cs673/pm/dto/Project.java
@@ -1,5 +1,74 @@
 package bu.met.cs.cs673.pm.dto;
 
-public class Project {
-
+import java.util.Date;
+/**
+ * 
+ * @author Luis Marion
+ */
+public class Project
+{
+	private int id;
+	private String name;
+	private String description;
+	private Date startDate;
+	private Date endDate;
+	private Date createDate;
+	private int createUser;
+	private Date lastModifiedDate;
+	private int lastModifiedUser;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public int getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(int createUser) {
+		this.createUser = createUser;
+	}
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+	public int getLastModifiedUser() {
+		return lastModifiedUser;
+	}
+	public void setLastModifiedUser(int lastModifiedUser) {
+		this.lastModifiedUser = lastModifiedUser;
+	}
 }
