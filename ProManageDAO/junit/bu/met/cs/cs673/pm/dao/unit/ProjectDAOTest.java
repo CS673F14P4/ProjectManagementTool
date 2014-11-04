9b7a9916b14bc393271bc5bb415cc286fc3b6267
Nethra Balasundaram <nethra.pd41@gmail.com>
2014-11-04 18:42:48 -0500

    UI
    tested UI

---
+++ ProManageDAO/junit/bu/met/cs/cs673/pm/dao/unit/ProjectDAOTest.java
@@ -0,0 +1,61 @@
+package bu.met.cs.cs673.pm.dao.unit;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import bu.met.cs.cs673.pm.dao.ProjectDAO;
+import bu.met.cs.cs673.pm.dto.Project;
+
+/**
+ * ProjectDAOTest
+ * 
+ * 
+ * @author Luis Marion
+ */
+public class ProjectDAOTest extends TestCase 
+{
+
+	private Project project;
+	
+	@Override
+    protected void setUp() throws Exception
+    {
+        project = new Project();
+        
+        project.setName("unitTestProject");
+        project.setDescription("This is a unit test project");
+        project.setStartDate(new java.util.Date());
+        project.setEndDate(new java.util.Date());
+        project.setCreateUser(1);
+        project.setLastModifiedUser(1);
+    }
+
+	@Test
+	public void testCreateProject()
+	{
+		ProjectDAO dao = new ProjectDAO();
+		int projectId = dao.createProject(project);
+		
+		System.out.println("id: " + projectId);
+		
+		assertTrue(projectId > 0);
+		
+		boolean success = dao.deleteProject(projectId);
+		assertTrue(success);
+	}
+	
+
+	@Test
+	public void testGetProject()
+	{
+		ProjectDAO dao = new ProjectDAO();
+		Project project = dao.getProject(1);
+		
+		System.out.println("project: " + project.getName());
+		
+		assertNotNull(project);
+	}
+	
+	
+}
