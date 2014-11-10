5e06ade0674077089825d1ad5ead149ea419e65c
daniel <danielcarlossmd@gmail.com>
2014-11-10 01:38:34 -0500

    Using a single page with ng-view to load the content of other pages inside.
    Using routers
    Put all the controllers inside app.js
    *Modal with problem to import scripts

--- ProManageUI/js/app.js
+++ ProManageUI/js/app.js
@@ -5,10 +5,19 @@
 	app.config(function($routeProvider) {
 		$routeProvider.when('/project', {
 			templateUrl : 'home.html',
-			controller : 'ApplicationController'
+			controller : 'TCtrl'
+		}).when('/project/:projectId', {
+			templateUrl : 'projectView.html',
+			controller : 'ProjectViewCtrl'
 		}).when('/signin', {
 			templateUrl : 'signin.html',
 			controller : 'ApplicationController'
+		}).when('/newproject', {
+			templateUrl : 'newproject.html',
+			controller : 'ApplicationController'
+		}).otherwise({
+			templateUrl : 'projectList.html',
+			controller : 'ProjectsController'
 		})
 	})
 
@@ -23,6 +32,120 @@
 		};
 	})
 
+	app.controller("ProjectViewCtrl", function($scope) {
+		$(".modalNewStory").load("modalNewStory.html");
+		$(".modalNewTask").load("modalNewTask.html");
+		$scope.test = "Oi gente";
+		$scope.project = {
+			"description" : "Bla bla bla about projetc A",
+			"name" : "Project A",
+			"idproject" : "1",
+			"status" : [ {
+				"name" : "Backlog",
+				"userStories" : [ {
+					"name" : "Story 1",
+					"description" : "Desc 1",
+					"tasks" : [ {
+						"name" : "task1",
+						"description" : "description task1",
+					}, {
+						"name" : "task1",
+						"description" : "description task1",
+					} ]
+				}, {
+					"name" : "Story 2",
+					"description" : "bla2",
+					"tasks" : [ {
+						"name" : "task1",
+						"description" : "description task1",
+					}, {
+						"name" : "task1",
+						"description" : "description task1",
+					} ]
+				} ]
+
+			}, {
+				"name" : "Current",
+				"userStories" : [ {
+					"name" : "Story 1",
+					"description" : "Desc 1",
+					"tasks" : [ {
+						"name" : "task1",
+						"description" : "description task1",
+					}, {
+						"name" : "task1",
+						"description" : "description task1",
+					} ]
+				} ]
+			}, {
+				"name" : "Done",
+				"userStories" : [ {
+					"name" : "Story 1",
+					"description" : "Desc 1",
+					"tasks" : [ {
+						"name" : "task1",
+						"description" : "description task1",
+					} ]
+				} ]
+			} ]
+
+		};
+
+	})
+
+	app.controller("ProjectsController", function($scope, $window) {
+
+		$scope.projects = [ {
+			"description" : "Bla bla bla about projetc A",
+			"name" : "Project A",
+			"idproject" : "1"
+		}, {
+			"description" : "Bla bla bla about projetc B",
+			"name" : "Project B",
+			"idproject" : "2"
+		}, {
+			"description" : "Bla bla bla about projetc C",
+			"name" : "Project C",
+			"idproject" : "3"
+		}, {
+			"description" : "Bla bla bla about projetc D",
+			"name" : "Project D",
+			"idproject" : "4"
+		} ];
+
+		$scope.message = "Projects";
+
+		// Get project id and update shared data
+		$scope.getProject = function(obj) {
+			var id = obj.currentTarget.attributes.data.nodeValue;
+			// redirect to project page
+			$window.location.href = '../html/projectview.html';
+		}
+	})
+
+	app.controller('UsersCtrl', function($scope) {
+		$scope.selected_item = 1;
+		$scope.users = [ {
+			"id" : "1",
+			"name" : "user1",
+		}, {
+			"id" : "2",
+			"name" : "user2",
+		}, {
+			"id" : "3",
+			"name" : "user3",
+		}, {
+			"id" : "4",
+			"name" : "user4",
+		}, {
+			"id" : "5",
+			"name" : "user5",
+		}, {
+			"id" : "6",
+			"name" : "nobody",
+		} ]
+	})
+
 	app.controller('LoginController', function($scope, $rootScope, $location,
 			AUTH_EVENTS, AuthService, $window) {
 		$scope.credentials = {
@@ -34,11 +157,11 @@
 			AuthService.login(credentials).then(function(user) {
 				$rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
 				$scope.setCurrentUser(user);
-				$rootScope.blau=user;
+				$rootScope.blau = user;
 				console.log(user);
 				$window.location.href = 'home.html';
 
-				//$location.path("/project");
+				// $location.path("/project");
 			}, function() {
 				$rootScope.$broadcast(AUTH_EVENTS.loginFailed);
 			});
@@ -63,8 +186,7 @@
 		var authService = {};
 
 		authService.login = function(credentials) {
-			return $http.post(
-					'http://localhost:8080/ProManageREST/jaxrs/user',
+			return $http.post('http://localhost:8080/ProManageREST/jaxrs/user',
 					credentials).then(
 					function(res) {
 						Session.create(res.data.id, res.data.user.id,
