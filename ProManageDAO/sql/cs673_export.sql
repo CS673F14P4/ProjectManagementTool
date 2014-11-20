238da1f642eacdf84a2bacb1b4178c65203dfc55
daniel <danielcarlossmd@gmail.com>
2014-11-20 15:29:08 -0500

    Removing a repeated line

--- ProManageDAO/sql/cs673_export.sql
+++ ProManageDAO/sql/cs673_export.sql
@@ -103,9 +103,6 @@ CREATE TABLE `story` (
   CONSTRAINT `idproject` FOREIGN KEY (`idproject`) REFERENCES `project` (`idproject`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
 
 /*!40101 SET character_set_client = @saved_cs_client */;
 
