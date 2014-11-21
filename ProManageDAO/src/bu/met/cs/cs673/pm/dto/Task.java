9e7fa5796044072e2418d76f13d78e81012f6816
raykg <raykg@bu.edu>
2014-11-21 16:08:13 -0500

    Test commit
    Trying github

--- ProManageDAO/src/bu/met/cs/cs673/pm/dto/Task.java
+++ ProManageDAO/src/bu/met/cs/cs673/pm/dto/Task.java
@@ -1,5 +1,16 @@
 package bu.met.cs.cs673.pm.dto;
 
 public class Task {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
