2d2562a52d0acff11239ca50520321c8b752caf6
daniel <danielcarlossmd@gmail.com>
2014-11-24 17:09:40 -0500

    File forgotten last commit

---
+++ ProManageREST/src/bu/met/cs/cs673/pm/jaxrs/mapper/TaskMapper.java
@@ -0,0 +1,39 @@
+package bu.met.cs.cs673.pm.jaxrs.mapper;
+
+import bu.met.cs.cs673.pm.jaxrs.model.Task;
+
+public class TaskMapper {
+
+	public static Task mapTask(final bu.met.cs.cs673.pm.dto.Task taskDTO) {
+		Task task = new Task();
+		
+		task.setId(taskDTO.getId());
+		task.setName(taskDTO.getName());
+		task.setDescription(taskDTO.getDescription());
+		task.setCreateDate(taskDTO.getCreateDate());
+		task.setDueDate(taskDTO.getDueDate());
+		task.setOwner(taskDTO.getOwner());
+		task.setLastModifiedDate(taskDTO.getLastModifiedDate());
+		task.setLastModifiedUser(taskDTO.getLastModifiedUser());
+
+		return task;
+	}
+
+	public static bu.met.cs.cs673.pm.dto.Task mapTask(final Task task)
+
+	{
+		bu.met.cs.cs673.pm.dto.Task taskDTO = new bu.met.cs.cs673.pm.dto.Task();
+
+		taskDTO.setId(taskDTO.getId());
+		taskDTO.setName(task.getName());
+		taskDTO.setDescription(task.getDescription());
+		taskDTO.setCreateDate(task.getCreateDate());
+		taskDTO.setDueDate(task.getDueDate());
+		taskDTO.setOwner(task.getOwner());
+		taskDTO.setLastModifiedDate(task.getLastModifiedDate());
+		taskDTO.setLastModifiedUser(task.getLastModifiedUser());
+
+		return taskDTO;
+	}
+
+}
